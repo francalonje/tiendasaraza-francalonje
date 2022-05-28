91361c83ba1ca51315b071ebb71564136f57670e
Francisco Calonje <calonjefran@gmail.com>
2022-05-28 15:47:55 -0300

    added typography and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import NavBar from './Components/NavBar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
 
 
 function App() {
   return (
-    <div className="App">
-     Hola
+    <div>
+      <ThemeProvider theme={theme}>
+        <Typography variant="body1" color="initial">
+          <NavBar></NavBar>
+          <Typography variant="h1" color="initial">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur commodi ullam ipsam iusto iure quaerat hic itaque minus cumque quibusdam pariatur quasi maiores, placeat natus doloremque. Nobis sint iure beatae.
+          </Typography>
+
+         </Typography>
+      </ThemeProvider>
     </div>
   );
 }
